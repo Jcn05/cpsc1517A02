893366afe1e76bf1b284482986f379541e2b1914
Js258 <8911242+Js258@users.noreply.github.com>
2019-09-20 12:09:00 -0600

    Update

--- OOPsSolution/OOPsReview/Program.cs
+++ OOPsSolution/OOPsReview/Program.cs
@@ -11,9 +11,8 @@ namespace OOPsReview
         static void Main(string[] args)
         {
             //assume that you know how to do data input into
-            //  a console application (CPSC1012)
+            //   a console application (CPSC1012)
             //the input data will simply be placed in a local variable
-            
             //linearlength
             double linearlength = 135.0;
             //height
@@ -22,36 +21,37 @@ namespace OOPsReview
             double width = 8.0;
             //style
             string style = "Neighbour friendly: Spruce";
-            //price
             double price = 108.00;
 
             //situation: have the required data for the class instance
-            //options a) create instance using default constructor AND 
+            //options a) create instance using default constructor AND
             //           multiple assignment statements
-            //        b) create instance using greedy constructor
+            //        b) create instance using greedie constructor
             //solution: b) one statement
 
             //FencePanel is a non-static class
-            //Console was a static class
+            //Console was a static class (DOES NOT store data)
             //for a non-static class, you MUST create an instance of the class
-            //      to be able to use the class
-            //syntax using the new keyword which requires a reference to he
-            //      appropriate class constructor
+            //     to be able to use the class
+            //syntax using the new keyword which requires a reference to the
+            //     appropriate class constructor
             FencePanel panel = new FencePanel(height, width, style, price);
 
-            //handle numerous gates
+            //handle numerious gates
             //create a class pointer able to reference the Gate class
+            //this pointer will be null
             Gate gateinfo;
 
             //a structure to hold a collection of Gate
-            //the structure to use should hold an unknown number of instances
+            //the stucture to use should hold an unknown number of instances
             //structure will be a List<T>
-            //create the new of the List<T> when it is defined
+            //create the new instance of the List<T> when it is defined
             List<Gate> gatelist = new List<Gate>();
 
             //assume you are in a loop to gather multiple gates
 
-            //technique 1 
+            //1st gate
+            //technique 1
             //create instance of Gate
             //collect data
             //add to List
@@ -60,7 +60,6 @@ namespace OOPsReview
             width = 4.0;
             style = "Neighbour friendly - 1/2 picket: spruce";
             price = 86.45;
-
             gateinfo.Height = height;
             gateinfo.Width = width;
             gateinfo.Style = style;
@@ -70,9 +69,8 @@ namespace OOPsReview
             //2nd gate
             //technique 2
             //collect data
-            //create instance of Gate
-            //add to List
-
+            //create the Gate instance
+            //add to list
             height = 6.25;
             width = 3.0;
             style = "Neighbour friendly: spruce";
@@ -80,12 +78,28 @@ namespace OOPsReview
             gateinfo = new Gate(height, width, style, price);
             gatelist.Add(gateinfo);
 
+
             //create the Estimate
-            Estimate ClientEstimate = new Estimate(); //system default. no constructors.
+            Estimate ClientEstimate = new Estimate();  //system default
             ClientEstimate.LinearLength = linearlength;
             ClientEstimate.Panel = panel;
             ClientEstimate.Gates = gatelist;
             ClientEstimate.CalculatePrice();
+
+            //ouptut client information
+            Console.WriteLine("The fence is to be a " + ClientEstimate.Panel.Style + " style");
+            Console.WriteLine("the linear fence length required is {0:0.0}", ClientEstimate.LinearLength);
+            Console.WriteLine("Number of required panels {0}",
+                ClientEstimate.Panel.EstimatedNumberOfPanels(ClientEstimate.LinearLength));
+            Console.WriteLine("NUmber of required gates {0}", ClientEstimate.Gates.Count);
+            double fenceArea = ClientEstimate.Panel.FenceArea(ClientEstimate.LinearLength);
+            foreach(var item in ClientEstimate.Gates)
+            {
+                fenceArea += item.GateArea();
+            }
+            Console.WriteLine(string.Format("Total fence surface area {0:0.00}", fenceArea * 2));
+            Console.ReadKey(); //required due to using just F5
+
         }
     }
 }
